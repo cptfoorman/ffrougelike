8f090bb4136dee195e90f9a3ffcb8e2291a5f45b
cptfoorman <rihousek01@gmail.com>
2025-01-13 13:35:42 +0100

    properly cloned Attacks
    fixed me not properly cloning attacks when adding through strategy

--- unit/UnitResouces/UnitUpgradeStrategies/AddAttackStrategy.gd
+++ unit/UnitResouces/UnitUpgradeStrategies/AddAttackStrategy.gd
@@ -4,5 +4,6 @@ class_name AddAttackStrategy
 @export var new_attack: UnitAttack
 
 func apply_strategy(unit: Unit):
-	unit.unitattacks.append(new_attack)
+	var new_attack_clone = new_attack.duplicate(true)
+	unit.unitattacks.append(new_attack_clone)
 	print("adding attack to "+unit.unitsstats.name)
